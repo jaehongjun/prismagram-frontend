c01eff681dcf86698544ff0687b1d52c9d3cc186
jaehongjun <gatsby0391@naver.com>
2019-04-20 23:02:13 +0900

    refactoring

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { gql } from "apollo-boost";
-import { ThemeProvider } from "styled-components";
-
+import styled, { ThemeProvider } from "styled-components";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
 import Router from "./Router";
 import { useQuery } from "react-apollo-hooks";
+import Footer from "./Footer";
 
 const QUERY = gql`
   {
@@ -13,16 +15,25 @@ const QUERY = gql`
   }
 `;
 
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: ${props => props.theme.maxWidth};
+  width: 100%;
+`;
+
 export default () => {
   const {
     data: { isLoggedIn }
   } = useQuery(QUERY);
+
   return (
     <ThemeProvider theme={Theme}>
-      <>
+      <Wrapper>
         <GlobalStyles />
         <Router isLoggedIn={isLoggedIn} />
-      </>
+        <Footer />
+        <ToastContainer position={toast.POSITION.BOTTOM_LEFT} />
+      </Wrapper>
     </ThemeProvider>
   );
 };
--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -2,16 +2,23 @@ import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
 export default createGlobalStyle`
-${reset}
-*{
-    box-sizing:border-box;
-}
-body{
-    background-color:${props => props.theme.bgColor};
-    color:${props => props.theme.blackColor};
-}
-a{
-    color:${props => props.theme.blueColor};
-    text-decoration:none;
-}
+    ${reset};
+    @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600,700');
+    * {
+        box-sizing:border-box;
+    }
+    body {
+        background-color:${props => props.theme.bgColor};
+        color:${props => props.theme.blackColor};
+        font-size:14px;
+        font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        padding-top: 140px;
+    }
+    a {
+        color:${props => props.theme.blueColor};
+        text-decoration:none;
+    }
+    input:focus{
+        outline:none;
+    }
 `;
