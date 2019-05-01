4ac5f360e6b63af95009d09d8a89caa2f5175c5b
jaehongjun <gatsby0391@naver.com>
2019-05-01 15:42:11 +0900

    comment setting

---
+++ src/Components/Post/PostContainer.js
@@ -0,0 +1,124 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import useInput from "../../Hooks/useInput";
+import PostPresenter from "./PostPresenter";
+import { useMutation, useQuery } from "react-apollo-hooks";
+import { TOGGLE_LIKE, ADD_COMMENT } from "./PostQueries";
+import { ME } from "../../SharedQueries";
+
+const PostContainer = ({
+  id,
+  user,
+  files,
+  likeCount,
+  isLiked,
+  comments,
+  createdAt,
+  caption,
+  location
+}) => {
+  const [isLikedS, setIsLiked] = useState(isLiked);
+  const [likeCountS, setLikeCount] = useState(likeCount);
+  const [currentItem, setCurrentItem] = useState(0);
+  const [selfComments, setSelfComments] = useState([]);
+  const comment = useInput("");
+  const { data: meQuery } = useQuery(ME);
+  const toggleLikeMutation = useMutation(TOGGLE_LIKE, {
+    variables: { postId: id }
+  });
+  const addCommentMutation = useMutation(ADD_COMMENT, {
+    variables: { postId: id, text: comment.value }
+  });
+  const slide = () => {
+    const totalFiles = files.length;
+    if (currentItem === totalFiles - 1) {
+      setTimeout(() => setCurrentItem(0), 3000);
+    } else {
+      setTimeout(() => setCurrentItem(currentItem + 1), 3000);
+    }
+  };
+  useEffect(() => {
+    slide();
+  }, [currentItem]);
+
+  const toggleLike = () => {
+    toggleLikeMutation();
+    if (isLikedS === true) {
+      setIsLiked(false);
+      setLikeCount(likeCountS - 1);
+    } else {
+      setIsLiked(true);
+      setLikeCount(likeCountS + 1);
+    }
+  };
+
+  const onKeyPress = e => {
+    const { which } = e;
+    if (which === 13) {
+      e.preventDefault();
+      comment.setValue("");
+      setSelfComments([
+        ...selfComments,
+        {
+          id: Math.floor(Math.random() * 1000),
+          text: comment.value,
+          user: {
+            username: meQuery.me.username
+          }
+        }
+      ]);
+      addCommentMutation();
+    }
+  };
+  return (
+    <PostPresenter
+      user={user}
+      files={files}
+      likeCount={likeCountS}
+      location={location}
+      caption={caption}
+      isLiked={isLikedS}
+      comments={comments}
+      createdAt={createdAt}
+      newComment={comment}
+      setIsLiked={setIsLiked}
+      setLikeCount={setLikeCount}
+      currentItem={currentItem}
+      toggleLike={toggleLike}
+      onKeyPress={onKeyPress}
+      selfComments={selfComments}
+    />
+  );
+};
+
+PostContainer.propTypes = {
+  id: PropTypes.string.isRequired,
+  user: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    avatar: PropTypes.string,
+    username: PropTypes.string.isRequired
+  }).isRequired,
+  files: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired
+    })
+  ).isRequired,
+  likeCount: PropTypes.number.isRequired,
+  isLiked: PropTypes.bool.isRequired,
+  comments: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired,
+      user: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        username: PropTypes.string.isRequired
+      }).isRequired
+    })
+  ).isRequired,
+  caption: PropTypes.string.isRequired,
+  location: PropTypes.string,
+  createdAt: PropTypes.string.isRequired
+};
+
+export default PostContainer;
